50698cc51dd1b4349dd0735bdfaa618d277bfd2f
Xavier Brigode <xavier.brigode@gmail.com>
2022-07-27 16:46:30 +0200

    WiP cards collection + database

--- assets/js/generateCard.js
+++ assets/js/generateCard.js
@@ -3,56 +3,56 @@ const repas_data = [
     {
       img: "assets/img/cone.png",
       name: "Salmon Cone",
-      description: "Blablabla",
+      description: "A thin slice of salmon and rice, wrapped in a seaweed leaf",
       price: 6.15,
       addInCart: "assets/img/cone.png",
     },
     {
       img: "assets/img/eggmaki.png",
       name: "Egg Maki",
-      description: "Blablabla",
+      description: "A delicate handful of fish roe wrapped in a seaweed leaf",
       price: 6.15,
       addInCart: "assets/img/cone.png",
     },
     {
       img: "assets/img/meatballs.png",
       name: "Meatballs",
-      description: "Blablabl",
+      description: "Our delicious meatball skewer duo",
       price: 6.15,
       addInCart: "assets/img/meatballs.png",
     },
     {
       img: "assets/img/noodle.png",
       name: "Noodle",
-      description: "Blablabla",
+      description: "The bowl of noodles (chicken, beef, shrimp), simple and perfect ",
       price: 6.15,
       addInCart: "assets/img/noodle.png",
     },
     {
       img: "assets/img/salmonmaki.png",
       name: "Salmon Maki",
-      description: "Blablabla",
+      description: "A tasty tuna maki ",
       price: 6.15,
       addInCart: "assets/img/salmonmaki.png",
     },
     {
       img: "assets/img/salmonsushi.png",
       name: "Salmon Sushi",
-      description: "blablabla",
+      description: "A delicate tuna or salmon sushi, the base of any good bento ",
       price: 6.15,
       addInCart: "assets/img/salmonsushi.png",
     },
     {
       img: "assets/img/shrimps.png",
       name: "Shrimps",
-      description: "blablabla",
+      description: "Fresh shrimp expertly spiced by the chef",
       price: 6.15,
       addInCart: "assets/img/shrimps.png",
     },
     {
       img: "assets/img/soup.png",
       name: "Soup",
-      description: "blablabla",
+      description: "Noodle broths, with fish, meat, vegetables, seaweed, egg, and miso or soy sauce seasoning...",
       price: 6.15,
       addInCart: "assets/img/soup.png",
     },
@@ -73,32 +73,41 @@ const repas_data = [
   ];
 
 const generateCard = (repas) => {
-    let card = document.createElement("section") // card
-    card = styleCard(card)
+    let card = document.createElement("section")
+    card.setAttribute("class", "card") // card
 
+    // IMAGE CONTAINER
     let areaImg = document.createElement("article")
     areaImg.style.width = "180px"
     areaImg.style.height = "150px"
     areaImg.style.overflow = "hidden"
-    areaImg.style.margin = "10px"
     areaImg.style.borderRadius = "15px"
-
+    areaImg.style.margin = "10px"
+    // IMAGE
     const img = document.createElement("img")
     img.setAttribute("src", repas.img)
     img.setAttribute("alt", "image du pays")
+    img.style.width = "200px"
+    img.style.height = "150px"
 
+    // TITLE + DESCRIPTION
     const infos = document.createElement("div")
     infos.style.display = "flex"
     infos.style.flexDirection = "column"
-
+    //  TITLE
     const titre = document.createElement("h2")
     titre.innerHTML = repas.name
     titre.style.textAlign = "center"
-    
+    //  DESCRIPTION
+    const description = document.createElement("p")
+    description.innerHTML = repas.description
+    description.style.textAlign = "center"
+
     card.appendChild(areaImg)
     areaImg.appendChild(img)
     card.appendChild(infos)
     infos.appendChild(titre)
+    infos.appendChild(description)
 
     return card;
 }
@@ -116,19 +125,5 @@ const createAllCards = (collection) => {
     }
 }
 
-const styleCard = (card) =>
-{
-    card.style.width = "200px"
-    card.style.height = "350px"
-    card.style.border = "solid"
-    card.style.display = "flex"
-    card.style.flexDirection = "column"
-    card.style.margin = "15px" 
-    card.style.borderRadius = "15px"
-    card.style.overflow = "hidden"
-    card.style.boxShadow = "5px 5px 5px #4d4d4d"
-
-    return card
-}
 
 window.onload = createAllCards(repas_data)

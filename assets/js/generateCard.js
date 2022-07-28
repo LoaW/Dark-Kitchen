ac1a405dbd438797a929e0c0f3912a3dd1a754b9
Xavier Brigode <xavier.brigode@gmail.com>
2022-07-28 14:10:59 +0200

    cards design amelioration

--- assets/js/generateCard.js
+++ assets/js/generateCard.js
@@ -52,21 +52,21 @@ const repas_data = [
     {
       img: "assets/img/soup.png",
       name: "Soup",
-      description: "Noodle broths, with fish, meat, vegetables, seaweed, egg, and miso or soy sauce seasoning...",
+      description: "Noodle broths, with fish, meat, vegetables, seaweed, egg, and miso or soy",
       price: 6.15,
       addInCart: "assets/img/soup.png",
     },
     {
       img: "assets/img/sushi.png",
       name: "Sushi",
-      description: "blablabla",
+      description: "Japanese dish made with seasoned rice accompanied by strips of raw fish.",
       price: 6.15,
       addInCart: "assets/img/sushi.png",
     },
     {
       img: "assets/img/vegemaki.png",
       name: "Vegemaki",
-      description: "blablabla",
+      description: "Dried nori seaweed roll surrounding white rice mixed with sweet rice vinegar",
       price: 6.15,
       addInCart: "assets/img/vegemaki.png",
     },
@@ -78,36 +78,46 @@ const generateCard = (repas) => {
 
     // IMAGE CONTAINER
     let areaImg = document.createElement("article")
-    areaImg.style.width = "180px"
-    areaImg.style.height = "150px"
-    areaImg.style.overflow = "hidden"
-    areaImg.style.borderRadius = "15px"
-    areaImg.style.margin = "10px"
+    areaImg.setAttribute("class", "areaImg")
     // IMAGE
     const img = document.createElement("img")
     img.setAttribute("src", repas.img)
     img.setAttribute("alt", "image du pays")
-    img.style.width = "200px"
-    img.style.height = "150px"
-
     // TITLE + DESCRIPTION
     const infos = document.createElement("div")
-    infos.style.display = "flex"
-    infos.style.flexDirection = "column"
+    infos.setAttribute("class", "infos")
     //  TITLE
     const titre = document.createElement("h2")
+    titre.setAttribute("class", "titre")
     titre.innerHTML = repas.name
-    titre.style.textAlign = "center"
     //  DESCRIPTION
     const description = document.createElement("p")
+    description.setAttribute("class", "description")
     description.innerHTML = repas.description
-    description.style.textAlign = "center"
+    // CARD FOOTER
+    const cardFooter = document.createElement("div")
+    cardFooter.setAttribute("class", "cardFooter")
+    // PRICE
+    const price = document.createElement("p")
+    price.setAttribute("class", "price")
+    price.innerHTML = repas.price
+    // ADD CART BUTTON
+    const addButton = document.createElement("img")
+    addButton.setAttribute("class", "addButton")
+    addButton.setAttribute("src", "./assets/img/add.png")
+
+
+
+
 
     card.appendChild(areaImg)
     areaImg.appendChild(img)
     card.appendChild(infos)
     infos.appendChild(titre)
     infos.appendChild(description)
+    card.appendChild(cardFooter)
+    cardFooter.appendChild(price)
+    cardFooter.appendChild(addButton)
 
     return card;
 }
@@ -115,9 +125,8 @@ const generateCard = (repas) => {
 // all Card
 const createAllCards = (collection) => {
     const container = document.querySelector(".main__collection")
-    container.style.display = "flex"
-    container.style.flexWrap = "wrap"
-    container.style.justifyContent = "center"
+    container.setAttribute("class", "container")
+   
 
     for (const repas of collection){
         const generatedCard = generateCard(repas)
